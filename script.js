4a1f4b425ea9cea966e4fba78e2d835371956427
zakyravaaditya-wq <zakyravaaditya@gmail.com>
2025-10-07 13:05:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementByld('sapaButton');
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function(){
   alert('Hallo! Terimakasih Atas Kunjungan Anda Pada Website Saya!');
 });
